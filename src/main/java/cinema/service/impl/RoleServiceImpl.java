8b318cbb74cb3b9decf8d747f2de972323b359eb
Alexsis Moskovchuk <alekssiuss@gmail.com>
2022-01-19 15:10:17 +0200

    spring part

---
+++ src/main/java/cinema/service/impl/RoleServiceImpl.java
@@ -0,0 +1,27 @@
+package cinema.service.impl;
+
+import cinema.dao.RoleDao;
+import cinema.model.Role;
+import cinema.model.RoleName;
+import cinema.service.RoleService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+    private final RoleDao roleDao;
+
+    public RoleServiceImpl(RoleDao roleDao) {
+        this.roleDao = roleDao;
+    }
+
+    @Override
+    public Role add(Role role) {
+        return roleDao.add(role);
+    }
+
+    @Override
+    public Role getByName(RoleName roleName) {
+        return roleDao.getByName(roleName)
+                .orElseThrow(() -> new RuntimeException("Can't find role " + roleName));
+    }
+}
